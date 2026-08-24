85b37563cf644279b5c448de8dfa164056b5bad7
frans <flphala@gmail.com>
2026-08-24 18:49:36 +0200

    Add SQL query to filter NYC taxi data

--- sql/cleaning_pipeline.sql
+++ sql/cleaning_pipeline.sql
@@ -1,1 +1,12 @@
+SELECT 
+    vendor_id, 
+    pickup_datetime, 
+    dropoff_datetime, 
+    passenger_count, 
+    trip_distance, 
+    fare_amount 
+FROM `nyc-taxi-data` 
+WHERE fare_amount > 0 
+  AND trip_distance > 0 
+  AND passenger_count BETWEEN 1 AND 6;
 
